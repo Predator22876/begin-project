90fde6bd101c1953a0d9f3b52b4019209e3912ef
Predator22876 <alienchose25@gmail.com>
2025-11-18 12:41:53 +1000

    add datamapper entity in repository

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -4,7 +4,7 @@ from repositories.hotels import HotelsRepository
 
 from src.database import async_session_maker
 from src.api.dependencies import PaginationDep
-from src.schemas.hotels import Hotel, HotelPATCH
+from src.schemas.hotels import HotelAdd, HotelPATCH
 
 
 router = APIRouter(prefix="/hotels", tags= ["Отели"])
@@ -26,7 +26,7 @@ async def get_hotels(
         )
             
 @router.post("")
-async def create_hotel(hotel_data: Hotel = Body(openapi_examples= {
+async def create_hotel(hotel_data: HotelAdd = Body(openapi_examples= {
     "1": {"summary": "Сочи", "value": {
         "title": "Отель Rich 5 звезд у моря",
         "location": "Сочи, ул Моря, 1", 
@@ -53,7 +53,7 @@ async def del_hotels(hotel_id: int):
 @router.put("/{hotel_id}")
 async def edit_hotel(
     hotel_id: int, 
-    hotel_data: Hotel
+    hotel_data: HotelAdd
 ):
     async with async_session_maker() as session:
         await HotelsRepository(session).edit(hotel_data, id=hotel_id)
